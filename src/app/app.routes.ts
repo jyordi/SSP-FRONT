9abc4604c3ab2f11a200d6335b7caa93cd05134a
Mildred Alely cruz Juarez <mildredalely07@gmail.com>
2026-02-28 21:14:25 -0600

    formularioseguimiento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { FormuPsico } from './civil/formu-psico/formu-psico';
+import { SeguimientoPsi } from './civil/seguimiento-psi/seguimiento-psi';
 
 export const routes: Routes = [
     {
         path: 'formu-psico',
         component: FormuPsico
+    },
+    {
+        path: 'segui-psi',
+        component: SeguimientoPsi
     }
 ];
