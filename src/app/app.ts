ff279db9a41bdd70352ada2e0aebc1a7f3032e18
[luis ngel] <olaquehace568@gmail.com>
2026-04-02 23:00:17 -0600

    diseño adaptado

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { NavbarComponent, NavbarConfig } from './voluntarios/shared/navbar/navbar';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, CommonModule],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  showNavbar: boolean = false;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe(() => {
+      this.showNavbar = this.router.url.includes('/voluntarios');
+    });
+  }
+
   navbarConfig: NavbarConfig = {
     brandName: 'Reconecta con la Paz',
     brandSubtitle: 'Gobierno del Estado de Oaxaca',
